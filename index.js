1d25e584b90735d141fb3ddf62f54d715d0fac27
INFINITY <nirajnimantha@gmail.com>
2023-07-15 17:31:47 +0530

    ava-update-with-db

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const { Client, Events, Partials, Collection, GatewayIntentBits } = require('discord.js');
 const config = require('./config/config');
 const colors = require("colors");
+const { fetchReactions } = require('./commands/slash/General/avatemplate');
+const initializeDatabase = require('./database/database');
+const mongoose = initializeDatabase();
 
 const client = new Client({
   intents: [
@@ -67,4 +70,62 @@ client.login(AuthenticationToken)
 process.on('unhandledRejection', async (err, promise) => {
   console.error(`[ANTI-CRASH] Unhandled Rejection: ${err}`.red);
   console.error(promise);
+});
+
+client.on('messageReactionAdd', async (user) => {
+  if(user.bot){
+    return;
+  }
+  // mongodb 
+  const db = mongoose.connection.useDb("AvaRaids");
+  const collectionmassage = db.collection("avaraids");
+  const messages = collectionmassage.find();
+    // For each message, check if it's still valid, and if so, continue to fetch reactions.
+    messages.forEach(async (messageData) => {
+      const now = new Date();
+      if (now < messageData.selectedDate) {
+          const channel = client.channels.cache.get(messageData.channelId); // replace with your channel ID
+          if (channel) {
+              const message = await channel.messages.fetch(messageData.messageId);
+              if (message) {
+                  // Continue to fetch reactions and process them as you did before
+                  fetchReactions(message);
+              }
+          }
+      } else {
+          // If the message is no longer valid, delete it from the database.
+          collectionmassage.deleteOne({ messageId: messageData.messageId }, function(err, res) {
+            if (err) throw err;
+          });
+      }
+  });
+});
+
+client.on('messageReactionRemove', async (user) => {
+  if(user.bot){
+    return;
+  }
+  // mongodb 
+  const db = mongoose.connection.useDb("AvaRaids");
+  const collectionmassage = db.collection("avaraids");
+  const messages = collectionmassage.find();
+    // For each message, check if it's still valid, and if so, continue to fetch reactions.
+    messages.forEach(async (messageData) => {
+      const now = new Date();
+      if (now < messageData.selectedDate) {
+          const channel = client.channels.cache.get(messageData.channelId); // replace with your channel ID
+          if (channel) {
+              const message = await channel.messages.fetch(messageData.messageId);
+              if (message) {
+                  // Continue to fetch reactions and process them as you did before
+                  fetchReactions(message);
+              }
+          }
+      } else {
+          // If the message is no longer valid, delete it from the database.
+          collectionmassage.deleteOne({ messageId: messageData.messageId }, function(err, res) {
+            if (err) throw err;
+          });
+      }
+  });
 });
